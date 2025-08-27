de28a8c239fc8ac4c702e2494f4a60a554fd4e71
ro-nald <daddsronald@gmail.com>
2025-08-27 12:07:49 +0100

    Configure a linux Azure Web App with the Python 3.11 runtime

--- main.tf
+++ main.tf
@@ -9,3 +9,15 @@ resource "azurerm_service_plan" "deployed_app_service_plan" {
   os_type             = "Linux"
   sku_name            = "F1"
 }
+resource "azurerm_linux_web_app" "example" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.deployed_app_service_plan.location
+  service_plan_id     = azurerm_service_plan.deployed_app_service_plan.id
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+}
