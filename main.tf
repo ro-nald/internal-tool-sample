51b6471609236abbaaeb81bf00e4d5c6ee052493
ro-nald <daddsronald@gmail.com>
2025-08-27 11:54:11 +0100

    Configure an app service plan

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "my-terraform-resource-group"
   location = var.deployment_azure_region
 }
+resource "azurerm_service_plan" "deployed_app_service_plan" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "app_name" {
+  description = "Name of the app to deploy. Default is 'example-app'"
+  type        = string
+  default     = "example-app"
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_-]{1,60}$", var.app_name))
+    error_message = "The resource name must be alphanumeric, contain dashes or underscores, and be between 1 and 60 characters long."
+  }
+}
+
 variable "deployment_azure_region" {
   description = "The Azure region in which to deploy. By default, this is 'westeurope' (West Europe)"
   type        = string
